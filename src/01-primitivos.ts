8ddbfa137a92c4dbc904a6415204dd0097bdb002
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-03-03 12:01:13 -0300

    feat: exercícios de primitivos

---
+++ src/01-primitivos.ts
@@ -0,0 +1,9 @@
+const estudante: string = "Isaac";
+const anoEntrada: number = 2020;
+const estaAtivo: boolean = true;
+
+console.log(estudante);
+console.log(anoEntrada);
+console.log(estaAtivo);
+
+// Typescript é uma linguagem de tipagem estática, o que significa que os tipos de dados são verificados em tempo de compilação.
